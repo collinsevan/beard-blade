a9db52446ac005026f75b0e87e0df88ece1d62c4
collinsevan <collinsevan33@gmail.com>
2025-03-30 00:46:24 +0000

    Add logout template, url and rulesets. Update button text and nav.

--- beardblade/urls.py
+++ beardblade/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from barber import views as barber_views
-from django.contrib.auth import views as auth_views   # noqa F401
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', barber_views.home, name='home'),
     path('base/', barber_views.base_view, name='base'),
     path('about/', barber_views.about, name='about'),
-    path("book/", barber_views.book_now, name="book_now"),
+    path('book/', barber_views.book_now, name='book_now'),
     path("accounts/register/", barber_views.register, name="register"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(
+        template_name="registration/logout.html"), name="logout"),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
